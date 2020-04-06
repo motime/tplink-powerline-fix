37ed717d442bcef089f768b2bcaef936ac2ded53
Moti Mendelovich <moti.mendelovich@elad-health.com>
2020-04-06 10:53:15 +0300

    initial commit. someday i will have the time to document this.

---
+++ PingService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace TplinkPowerlineFix
+{
+    internal class PingService
+    {
+        public async Task Start()
+        {
+            var defaultGateway = GetDefaultGateway();
+            if (defaultGateway == null)
+                throw new Exception("Default gateway not found");
+
+            await PingForeverAsync(defaultGateway);
+        }
+
+        public async Task PingForeverAsync(IPAddress host)
+        {
+            Ping ping = new Ping();
+            while (true)
+            {
+                await Task.Delay(5000);
+                var pingReply = await ping.SendPingAsync(host, 4000);
+            }
+        }
+
+        public static IPAddress GetDefaultGateway()
+        {
+            return NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up)
+                .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(n => n.GetIPProperties()?.GatewayAddresses)
+                .Select(g => g?.Address)
+                // .Where(a => a.AddressFamily == AddressFamily.InterNetwork)
+                // .Where(a => Array.FindIndex(a.GetAddressBytes(), b => b != 0) >= 0)
+                .FirstOrDefault(a => a != null);
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Topshelf;
+
+namespace TplinkPowerlineFix
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<PingService>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => new PingService());                //3
+                    s.WhenStarted(async pingService => await pingService.Start());
+                    s.WhenStopped(pingService => pingService.Stop());    
+                    //4
+                                                                                      //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("https://www.youtube.com/watch?v=Al3kwmfzsOQ");                   //7
+                x.SetDisplayName("Tplink Powerline Fix");                                  //8
+                x.SetServiceName("Tplink.Powerline.Fix");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
